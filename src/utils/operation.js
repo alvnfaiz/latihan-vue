e3d40c15a7b96cc388e29a76fbbfdc31e525ea75
Alvin Faiz <softkopi3258@gmail.com>
2022-04-25 17:13:43 +0700

    add jest

---
+++ src/utils/operation.js
@@ -0,0 +1,10 @@
+export const penjumlahan = (var1, var2) => var1 + var2;
+export const pengurangan = (var1, var2) => var1 - var2;
+export const perkalian = (var1, var2) => var1 * var2;
+export const pembagian = (var1, var2) => var1 / var2;
+export const rata_rata = (var1, var2, var3, var4, var5) => (var1 + var2 + var3 + var4 + var5) / 5;
+export const sisa_bagi = (var1, var2) => var1 % var2;
+export const luas_persegi = (var1) => var1 * var1;
+export const luas_lingkaran = (var1) => 3.14 * var1 * var1;
+export const luas_kubus = (p, l, t) => 2(p * l * t);
+export const volume_kubus = (p, l, t) => p * l * t;
