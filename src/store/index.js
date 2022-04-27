0f8b75824d5797d3552fe480e6e4277358d24e3f
Alvin Faiz <softkopi3258@gmail.com>
2022-04-27 14:58:13 +0700

    berita store

--- src/store/index.js
+++ src/store/index.js
@@ -7,4 +7,23 @@ export default new Vuex.Store({
     state: {
         berita: [],
         kategori: [],
-        
+    },
+    mutations: {
+        setBerita(state, payload) {
+            state.berita = payload
+        }
+    },
+    actions: {
+        getBerita({ commit }) {
+            axios.get('/api/berita')
+                .then(res => {
+                    commit('setBerita', res.data)
+                })
+        }
+    },
+    getters: {
+        getBerita(state) {
+            return state.berita
+        }
+    }
+})
